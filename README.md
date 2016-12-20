54d10e54abebecb921d3bdb2b037f86f87165bca
Archit Kapoor <archit@Archits-MBP.lan>
2016-12-20 12:40:27 +0530

    Modifying gitignore and README files

--- README.md
+++ README.md
@@ -3,7 +3,10 @@ Repository of my solutions to competitive programming questions.
 
 The respository does not contains solutions from just one specific site, but it's rather mixed.
 
+Better solutions to my programs are welcomed. Code optimizations are welcomed. Failing test-cases are most welcomed.
+
 Sites I usually use to fetch programming questions:
 
 [Hackerearth](https://www.hackerearth.com/)
+
 [Hackerrank](https://www.hackerrank.com/)
---
+++ src/Prefix Compression.scala
@@ -0,0 +1,34 @@
+/*
+
+@Title: Prefix Compression
+
+@Problem Statement: 
+
+You are in charge of data transfer between two Data Centers. Each set of data is represented by a pair of strings. 
+Over a period of time you have observed a trend: most of the times both strings share some prefix. 
+You want to utilize this observation to design a data compression algorithm which will be used to reduce amount of data to be transferred.
+
+You are given two strings, and , representing the data, you need to find the longest common prefix () of the two strings. 
+Then you will send substring , and , where and are the substring left after stripping from them. 
+
+[....]
+
+@URL: https://www.hackerrank.com/contests/lambda-calculi-oct14/challenges/prefix-compression
+*/
+
+object Main extends App {
+	
+	var words = List[String]() 
+	for(ln <- io.Source.stdin.getLines) words = ln :: words
+	
+	val prefix = words(0).zip(words(1)).takeWhile(t=> t._1== t._2).map(_._1).mkString
+
+	val prefixLength = prefix.length
+	val xd = words(1).substring(prefixLength)
+	val yd = words(0).substring(prefixLength)
+
+	println(prefixLength + " " + prefix)
+	println(xd.length + " " + xd)
+	println(yd.length + " " + yd)
+
+}
